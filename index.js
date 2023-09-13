e6aba6e2810049ac2bd3cebcc3af702381994174
Emilse Arias <emilsearias@gmail.com>
2023-09-13 19:04:17 -0300

    agregue la carpeta publica

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ import 'dotenv/config'; //permite procesar variables de entorno
 import productoRouter from './src/routes/productos.routes.js';
 import usuarioRouter from './src/routes/usuario.routes.js';
 import './src/database/database.js';
+import path from "path";
+import { fileURLToPath } from 'url';
 
 // 1- configuraciones iniciales
 const app = express();
 // crear una variable
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 4001);
 app.listen(app.get('port'), ()=>{
     console.log('Estoy en el puerto '+app.get('port') )
 })
@@ -19,6 +21,10 @@ app.use(cors()); //permite conexiones remotas
 app.use(express.json());//permite interpretar datos en formato json
 app.use(express.urlencoded({extended:true})); // ayuda a interpretar datos del body del request
 app.use(morgan('dev')); //nos da mas informacion en la terminal
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname)
+app.use(express.static(path.join(__dirname, "/public")));
 
 // 3- crear las rutas
 // http://localhost:4000/api/producto
